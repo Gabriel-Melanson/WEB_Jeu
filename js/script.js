1753b837e51dfe6cafefaf852eac1b371abeaa73
Gabriel M <gabriel.melanson25@gmail.com>
2023-12-17 22:24:46 -0500

    centrer le canevas, commente le js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ let touchesClavier = {
     "ArrowDown": false,
     "ArrowLeft": false,
     "ArrowRight": false
-
 };
 
 const VITESSE_JOUEUR = 10;
@@ -28,26 +27,31 @@ const LARGEUR_JOUEUR = 35;
 const HAUTEUR_JOUEUR = 35;
 const DIMENSION_OBJET = 50;
 
+//fonction intiale qui commence le projet
 window.onload = function () {
     //obtient element texte avec font qu'on peut alors utiliser
     document.getElementById("chargeTexte").style.display = "none";
 
+    //activation des listener d'evenements qui traquent les inputs des touches
     window.addEventListener("keydown", toucheAppuyee);
     window.addEventListener("keyup", toucheRelachee);
 
+    //obtention du canevas et des dimensions du canevas
     canevas = document.getElementById('ZoneJeu');
     contexte = canevas.getContext('2d');
-    contexte.canvas.width = window.innerWidth - window.innerWidth * 0.01;
-    contexte.canvas.height = window.innerHeight - window.innerHeight * 0.01;
+    contexte.canvas.width = window.innerWidth;
+    contexte.canvas.height = window.innerHeight;
 
-    p1X = (canevas.width - LARGEUR_JOUEUR) / 2;
+    let distanceCentre = 200;
+    p1X = (canevas.width - LARGEUR_JOUEUR) / 2 + distanceCentre;
     p1Y = (canevas.height - HAUTEUR_JOUEUR) / 2;
-    p2X = (canevas.width - LARGEUR_JOUEUR) / 2;
+    p2X = (canevas.width - LARGEUR_JOUEUR) / 2 - distanceCentre;
     p2Y = (canevas.height - HAUTEUR_JOUEUR) / 2;
 
     window.requestAnimationFrame(boucleJeu);  // le navigateur appellera boucleJeu() au bon moment
 }
 
+//evenement qui active lors de l'appui de touches
 function toucheAppuyee(evenement) {
     console.log("touche appuyée: " + evenement.key);
     switch (evenement.key) {
@@ -78,6 +82,7 @@ function toucheAppuyee(evenement) {
     }
 }
 
+//evenement qui active lorsque les touches sont levees
 function toucheRelachee(evenement) {
     switch (evenement.key) {
         case "ArrowUp":
@@ -107,6 +112,7 @@ function toucheRelachee(evenement) {
     }
 }
 
+//boucle centrale du jeu qui joue a chaque frame
 function boucleJeu(timeStamp){
     calculerPosition();
     dessiner();
@@ -114,19 +120,21 @@ function boucleJeu(timeStamp){
     window.requestAnimationFrame(boucleJeu);  // le navigateur appellera boucleJeu() au bon moment
 }
 
+let ass = 0;
 function calculerPosition(){
-    console.log();
+    ass++;
+    console.log(ass);
 }
 
 function dessiner() {
-    console.log("0");
-    dessinerBG();
-    // effacer le canevas
+    // effacer le contenu de la frame precedente
     contexte.clearRect(0, 0, canevas.width - 1, canevas.height - 1);
 
-    // affiche le carré
+    // affiche un bg orange
     contexte.fillStyle = "orange";
     contexte.fillRect(0, 0, canevas.width - 1, canevas.height - 1);
+
+    dessinerBG();
 
 
 }
@@ -135,6 +143,8 @@ function dessinerBG(){
     let repeatX = Math.round(contexte.canvas.width / 10);
     let repeatY = Math.round(contexte.canvas.width / 10);
     for(let indexX = 0; indexX < repeatX; indexX++){
+        for(let indexY = 0; indexY < repeatY; indexY++){
 
+        }
     }
 }
